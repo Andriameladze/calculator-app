c29d4d9204216df2c16a9bd55517cbf32cc98274
Andria Meladze <andriameladze@Andrias-MacBook-Air.local>
2023-01-17 19:30:10 +0400

    add basic logic

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss']
+  styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent {
+  showValue = '0';
+  currentValue = '';
 
+  inputValue(value: string) {
+    if (this.currentValue == '0') {
+      this.currentValue = value;
+    } else if (this.currentValue != '0') {
+      this.currentValue = this.currentValue + value;
+    }
+    this.showValue = this.currentValue;
+  }
+
+  clearValue() {
+    this.showValue = '0';
+    this.currentValue = '';
+  }
+
+  negativeToggle() {
+    this.currentValue = `${+this.currentValue * -1}`;
+    this.showValue = this.currentValue;
+  }
+
+  equals() {
+    this.showValue = eval(this.currentValue);
+    this.currentValue = this.showValue;
+  }
 }
